9984ca67d9fc7f5c71035f68700f09a0b658b828
Ester Diaz <ester.diaz.sola@gmail.com>
2018-09-28 12:26:28 +0200

    Build/Destroy Screens

---
+++ main.js
@@ -0,0 +1,86 @@
+function buildDom(html) {
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  return div.children[0];
+}
+
+function main() {
+
+  var mainContainerElement = document.querySelector('#main-container');
+  
+  // -- Splash
+  var splashElement = null;
+  var splashButton = null;
+
+  var handleSplashClick = function () {
+    destroySplash();
+    buildGame();
+  }
+
+  function buildSplash() {
+    splashElement = buildDom(`
+      <main class="splash container">
+        <h1 class="splash__title">Chain Reaction</h1>
+        <button>Start</button>
+      </main>
+    `)
+    mainContainerElement.appendChild(splashElement);
+    
+    splashButton = document.querySelector('button');
+    splashButton.addEventListener('click', handleSplashClick)
+
+  }
+  function destroySplash() {
+    splashButton.removeEventListener('click', handleSplashClick);
+    splashElement.remove();
+  }
+
+  // -- Game
+  var game = null;
+  var handleGameOver = function () {
+    destroyGame();
+    buildGameover();
+  };
+
+  function buildGame() {
+    setTimeout(() => {
+      handleGameOver();
+    }, 2000);
+  }
+
+  function destroyGame() {
+  }
+
+  
+  // -- Gameover
+  var gameoverElement = null;
+  var gameoverButton = null;
+
+  var handleGameoverClick = function () {
+    destroyGameover();
+    buildGame();
+  }
+
+  function buildGameover() {
+    gameoverElement = buildDom(`
+      <main class="gameover container">
+        <h1>Game Over</h1>
+        <button>Restart</button>
+      </main>
+    `);
+    mainContainerElement.appendChild(gameoverElement);
+
+    gameoverButton = document.querySelector('button');
+    gameoverButton.addEventListener('click', handleGameoverClick);
+
+  }
+
+  function destroyGameover() {
+    gameoverButton.removeEventListener('click', handleGameoverClick);
+    gameoverElement.remove();
+  }
+
+  buildSplash();
+}
+
+document.addEventListener('DOMContentLoaded', main);
